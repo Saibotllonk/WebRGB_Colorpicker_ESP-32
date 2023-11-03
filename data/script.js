5d7af216a882e4732d87237bdae1d5a5e430a032
Tobias Knoll <tobiasknoll2003@protonmail.com>
2023-11-03 14:22:38 +0100

    first commit

---
+++ data/script.js
@@ -0,0 +1,110 @@
+
+var Socket;
+
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+
+    swatches: [
+        'rgba(255, 0, 0, 1)',
+        'rgba(0, 255, 0, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(255, 0, 119, 1)',
+        'rgba(37, 13, 215, 1)',
+        'rgba(33, 150, 243, 1)',
+        'rgba(3, 255, 180, 1)',
+        'rgba(139, 195, 74, 1)',
+        'rgba(205, 220, 57, 1)',
+        'rgba(255, 193, 7, 1)',
+       'rgba(126, 95, 3, 1)',
+        'rgba(255, 117, 0, 1)',
+        'rgba(255, 255, 255, 1)',
+        'rgba(0, 0, 0, 1;)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: false,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
+});
+
+pickr.on('save', color => {
+    let color_hex;
+    if(color == null){
+        color_hex = "#000000";
+    }else{
+    color_hex = color.toHEXA().toString();
+    }
+   color_changed(color_hex);
+})
+
+let color_picker_button = document.querySelector(".pcr-button");
+let panel = document.getElementById("panel");
+let slider = document.getElementById("LED_INTENSITY");
+let slider_value = document.getElementById("LED_VALUE");
+
+slider.addEventListener("input", slider_changed);
+
+function init(){
+    //create Websocket
+    Socket = new WebSocket('ws://' + window.location.hostname +':81/');
+
+    color_picker_button.style.setProperty("height", "5em");
+    color_picker_button.style.setProperty("width", "5em");
+    
+    //run on Client Request
+    Socket.onmessage = function(event){
+        //get Json Obj from sendet Data
+        var object = JSON.parse(event.data);
+        var type = object.type;
+        if(type.localeCompare("LED_INTENSITY") == 0){
+            let l_led_intensity = object.value;
+            console.log(l_led_intensity);
+            slider.value = l_led_intensity;
+            slider_value.innerHTML = l_led_intensity;
+        }
+        else if(type.localeCompare("LED_COLOR") == 0){
+           let l_led_color = object.value;
+           console.log(l_led_color);
+           color_picker_button.style.setProperty("--pcr-color", l_led_color);
+        }
+       
+    }
+}
+//send staus 
+function color_changed(color){
+    //create status_json
+   var msg = {
+    type: "LED_COLOR",
+    value: color
+   }
+    //parse staus in Json Obj and send to Socket
+    Socket.send(JSON.stringify(msg));
+    console.log(msg);
+}
+
+function slider_changed(){
+    var l_led_intensity = slider.value;
+    console.log(l_led_intensity);
+    var msg = {
+        type: "LED_INTENSITY",
+        value: l_led_intensity
+    }
+    Socket.send(JSON.stringify(msg));
+    
+}
+
+//run init at start
+window.onload = function(event){
+    init();
+}
---
+++ src/main.cpp
@@ -0,0 +1,247 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WebSocketsServer.h>
+#include <ArduinoJson.h>
+#include <LittleFS.h>
+
+#define LED_R 17
+#define LED_G 16
+#define LED_B 4
+
+#define WIFI_SSID "WIFI_SSID..."
+#define WIFI_PASS "WIFI_PASSWORD..."
+
+void webSocketEvent(byte num, WStype_t type, uint8_t *payload, size_t length);
+void send_update(String type, int value);
+String hex_to_String(int hex);
+int String_to_hex(char *hex_str);
+void writetoRGB();
+
+WebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
+IPAddress ServerIP;
+
+StaticJsonDocument<200> doc_rx;
+StaticJsonDocument<200> doc_tx;
+
+int color = 0x000000;
+int RGB[] = {0, 0, 0};
+int intensity = 50;
+
+// for LED analog write SETUP
+const int freq = 5000;
+const int led_channels[] = {1, 2, 3};
+const int resolution = 8;
+
+void setup()
+{
+  // LED analog Write Setup
+  ledcSetup(led_channels[0], freq, resolution);
+  ledcSetup(led_channels[1], freq, resolution);
+  ledcSetup(led_channels[2], freq, resolution);
+
+  ledcAttachPin(LED_R, led_channels[0]);
+  ledcAttachPin(LED_G, led_channels[1]);
+  ledcAttachPin(LED_B, led_channels[2]);
+
+  // LED pinMode setup
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+
+  Serial.print("starting Server");
+
+  LittleFS.begin();
+
+  // Setup INTRANET Acces
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(250);
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+  ServerIP = WiFi.localIP();
+  Serial.print("\nIP-Adress: ");
+  Serial.println(ServerIP);
+
+  // URL/
+  File webpage = LittleFS.open("/index.html", "r");
+  String webpage_str = webpage.readStringUntil(EOF);
+  webpage.close();
+
+  server.on("/", [webpage_str]()
+            { server.send(200, "text/html", webpage_str); });
+
+  // URL/script.js
+  File script = LittleFS.open("/script.js", "r");
+  String script_str = script.readStringUntil(EOF);
+  script.close();
+
+  server.on("/script.js", [script_str]()
+            { server.send(200, "text/javascript", script_str); });
+
+  // URL/fade
+  File webpage_fade = LittleFS.open("/fade/fade.html", "r");
+  String webpage_fade_str = webpage_fade.readStringUntil(EOF);
+  webpage_fade.close();
+
+  server.on("/fade", [webpage_fade_str]()
+            { server.send(200, "text/html", webpage_fade_str); });
+
+  // URL/fade/script_fade.js
+  File script_fade = LittleFS.open("/fade/script_fade.js", "r");
+  String script_fade_str = script_fade.readStringUntil(EOF);
+  script_fade.close();
+
+  server.on("/fade/script_fade.js", [script_fade_str]
+            { server.send(200, "text/javascript", script_fade_str); });
+
+  server.begin();
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+}
+
+bool connected = false;
+
+void loop()
+{
+  webSocket.loop();
+  // handle Client Request
+  server.handleClient();
+  // keep status up to date
+  if (WiFi.status() == WL_CONNECTED && !connected)
+  {
+    // Serial.println("connected");
+    digitalWrite(LED_BUILTIN, HIGH);
+    connected = true;
+  }
+
+  else if (WiFi.status() != WL_CONNECTED)
+  {
+    // Serial.print(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    connected = false;
+    delay(1000);
+  }
+}
+
+void webSocketEvent(byte num, WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.println("Client Disconnected");
+    break;
+  case WStype_CONNECTED:
+    Serial.println("Client Connected");
+    send_update("LED_COLOR", color);
+    send_update("LED_INTENSITY", intensity);
+    break;
+  case WStype_TEXT:
+    DeserializationError error = deserializeJson(doc_rx, payload);
+    if (error)
+    {
+      Serial.println("deserialization failed.");
+      return;
+    }
+    const char *l_type = doc_rx["type"];
+
+    if (String(l_type) == "LED_INTENSITY")
+    {
+      const int value = doc_rx["value"];
+      intensity = int(value);
+      send_update("LED_INTENSITY", intensity);
+      if (color != 0) // every LED off -> skip
+      {
+        writetoRGB();
+      }
+    }
+    else if (String(l_type) == "LED_COLOR")
+    {
+      String value = doc_rx["value"];
+      char *value_c = new char[value.length() + 1];
+      strcpy(value_c, value.c_str());
+
+      color = String_to_hex(value_c);
+
+      delete[] value_c;
+
+      send_update("LED_COLOR", color);
+
+      // set new RGB values
+      RGB[0] = ((color >> 16) & 0xFF);
+      RGB[1] = ((color >> 8) & 0xFF);
+      RGB[2] = ((color)&0xFF);
+
+      Serial.printf("\nColor:\n\tr: %i\n\tg: %i\n\tb: %i\n", RGB[0], RGB[1], RGB[2]);
+
+      writetoRGB();
+    }
+    else
+    {
+      Serial.println("type not found!");
+    }
+
+    break;
+  }
+}
+
+// sends Msg with included Values to Clients connected to WebSocket  (Types: LED_COLOR, LED_INTENSITY)
+void send_update(String type, int value)
+{
+  String jsonString = "";
+  JsonObject object = doc_tx.to<JsonObject>();
+  // get cur status
+  object["type"] = type;
+  object["value"] = value;
+  // for Color Hex String needed in Format: #RRGGBB
+  if (type == "LED_COLOR")
+  {
+    object["value"] = hex_to_String(value);
+  }
+  serializeJson(doc_tx, jsonString);
+
+  Serial.println(jsonString);
+  // send JsonString to Websocket
+  webSocket.broadcastTXT(jsonString);
+}
+
+String hex_to_String(int hex)
+{
+  // Serial.printf("hex_to_String:\n\tint_hex: %x\n\t", hex);
+  char hex_str[] = "#000000";
+  sprintf(hex_str, "#%.6x", hex);
+  // Serial.printf("str_hex: %s\n", hex_str);
+  return String(hex_str);
+}
+
+int String_to_hex(char *hex_str)
+{
+  // Serial.printf("String_to_hex:\n\tstr_hex: %s\n\t", hex_str);
+  if (hex_str[0] == '#')
+    hex_str++;
+  int hex = (int)strtol(hex_str, NULL, 16);
+  // Serial.printf("int_hex: %x\n", hex);
+  return hex;
+}
+
+// writes RGB[] values combined with intensity(+PWM) to Output
+void writetoRGB()
+{
+  for (int i = 0; i < 3; i++)
+  {
+    if (RGB[i] != 0) // single LED off -> skip
+    {
+      // Serial.printf("old Color: %i\n", RGB[i]);
+      int dimmed_color = (int)(RGB[i] * (intensity / 100.0f));
+      // Serial.printf("nes Color: %i", dimmed_color);
+      ledcWrite(led_channels[i], dimmed_color);
+    }
+    else
+    {
+      ledcWrite(led_channels[i], 0);
+    }
+  }
+}
